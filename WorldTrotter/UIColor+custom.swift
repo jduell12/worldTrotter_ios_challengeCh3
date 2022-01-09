cdf271594795a9f144617c2fa0f54f1178f300a4
Jessica <duellje@oregonstate.edu>
2022-01-09 17:48:40 -0500

    Initial Commit

---
+++ WorldTrotter/UIColor+custom.swift
@@ -0,0 +1,31 @@
+//
+//  UIColor+custom.swift
+//  WorldTrotter
+//
+//  Created by Jessica  Duell on 1/9/22.
+//
+
+import UIKit
+
+extension UIColor {
+    struct Custom {
+        struct Blue {
+            static let Teal = UIColor(rgb: 0x29B2E1)
+            static let Aqua = UIColor(rgb: 0x1A93BD)
+        }
+        
+        struct Green {
+            static let LimeGreen = UIColor(rgb: 0x3AD42B)
+        }
+        
+        struct Red {
+            static let Pink = UIColor(rgb: 0xC52BD4)
+        }
+        
+        struct Grey {
+            static let GreyBrown = UIColor(rgb: 0xF5F4F1)
+            static let DarkGrey = UIColor(rgb: 0xF1F2F5)
+            static let Grey = UIColor(rgb: 0x6D7892)
+        }
+    }
+}
